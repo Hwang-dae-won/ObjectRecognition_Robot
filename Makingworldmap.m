90627627654850164131c983a311c8ecb39847de
Hwang-dae-won <skcskc7@gmail.com>
2016-12-08 22:31:03 +0900

    Update Makingworldmap.m

--- Makingworldmap.m
+++ Makingworldmap.m
@@ -14,26 +14,3 @@ C = ExampleHelperGazeboCommunicator();
 
 %% Spawn My home and My households
 
-%Home
-home           = ExampleHelperGazeboModel('My_Room1_h','gazeboDB');
-
-%Living room
-Tv_table_h           = ExampleHelperGazeboModel('My_Tv_table_h','gazeboDB');
-Sofa1_h           = ExampleHelperGazeboModel('My_Sofa1_h','gazeboDB');
-Coffee_table1_h          = ExampleHelperGazeboModel('My_Coffee table1_h','gazeboDB');
-
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-%Home
-spawnModel(C,home,                 [0,             0,          0]);
-
-%Living room
-spawnModel(C,Tv_table_h,          [6560,             1400,          0]/1000);
-spawnModel(C,Sofa1_h,          [10060,             1400,          0]/1000);
-spawnModel(C,Coffee_table1_h,          [8660,             1400,          0]/1000);
-
-
-
-%% Disconnect from the ROS
-
